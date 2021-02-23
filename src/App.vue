31ec014988668d2565effc223d927badfc95427e
Ganesh kolavennu <50847468+macklark@users.noreply.github.com>
2021-02-23 18:47:52 +0530

    initial main component

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+    <router-view></router-view>
+</template>
+
+<script>
+import About from "@/views/About.vue"
+import Dashboard from "@/views/Dashboard.vue"
+import Notauth from "@/views/Notauth.vue"
+import Newform from "@/views/Newform.vue"
+import Yourblogs from "@/views/Yourblogs.vue"
+    export default{
+        name : "App",
+        components : {About, Dashboard, Notauth, Yourblogs, Newform}
+    }
+</script>
