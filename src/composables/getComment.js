71f7c298910663ea5bafe4608a32ba52d06fadb8
Ganesh kolavennu <50847468+macklark@users.noreply.github.com>
2021-03-04 19:09:50 +0530

    Create getComment.js

---
+++ src/composables/getComment.js
@@ -0,0 +1,33 @@
+import { firestoreStorage } from "../firebase/config"
+import { ref } from "vue"
+
+const getComment = (id) => {
+    const comments = ref([])
+    const error = ref(null)
+
+    const loader = async () => {
+        try{
+            let res = await firestoreStorage.collection("comments").where("post", "==", id).onSnapshot((snap) => {
+                let docs = snap.docs.map(doc => {
+                    return{
+                        ...doc.data(), id : doc.id
+                    }
+                })
+                comments.value = docs
+
+            })
+
+            if(!res.exists){
+                throw Error("comment does not exist !")
+            }
+        }catch(err){
+            error.value = err.message
+        }
+    }
+
+    return{
+        comments, loader, error
+    }
+}
+
+export default getComment
